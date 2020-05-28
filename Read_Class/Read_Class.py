16ea127a8a1d34a49778f7cb191c7872a11b9c6a
Rohit Chauhan <root@vitblruatmlv2.viteos.com>
2020-05-28 20:21:20 +0530

    made some changes in Read_Class to make a query out of columns

---
+++ Read_Class/Read_Class.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+""" 
+Created on Tue May 28 19:35:29 2020
+
+@author: consultant138
+"""
+import json
+
+class Read_Class:
+	
+	def __init__(self, param_columns_path = '../columns_json.json', param_add_columns_path = '../add_columns_json.json', param_default_ssh_path = '../default_ssh.json', param_default_mongo_path = '../default_mongo.json'):
+	
+		with open(param_columns_path) as infile:
+			self.columns_from_json = json.load(infile)
+		self.columns_list = json.loads(self.columns_from_json)
+		
+		with open(param_add_columns_path) as infile:
+			self.add_columns_from_json = json.load(infile)
+		self.add_columns_list = json.loads(self.add_columns_from_json)
+		
+		with open(param_default_ssh_path) as infile:
+			self.default_ssh_from_json = json.load(infile)
+		
+
+		with open(param_default_mongo_path) as infile:
+			self.default_mongo_from_json = json.load(infile)
+		
+				
+		self.all_columns_list = json.load(self.columns_from_json) + json.load(self.add_columns_from_json
+
+	def columns_list(self):
+		return self.columns_list
+
+	def add_columns_list(self):
+		return self.add_columns_list
+	
+	def columns_json(self):
+		return self.columns_from_json
+
+	def add_columns_json(self):
+		return self.add_columns_from_json
+
+	def all_columns_list(self):
+		return self.all_columns_list
+
+	def all_columns_json(self):
+		return self.columns_from_json + self.add_columns_from_json
+
+	def all_columns_query(self):
+		
+	def ssh_host(self):
+		return self.default_ssh_from_json['DEFAULT_SSH_HOST']
+
+	def ssh_port(self):
+		return self.default_ssh_from_json('DEFAULT_SSH_PORT')
+
+	def ssh_username(self):
+		return self.default_ssh_from_json('DEFAULT_SSH_USERNAME')
+
+	def ssh_password(self):
+		return self.default_ssh_from_json('DEFAULT_SSH_PASSWORD')
+
+	def mongo_host(self):
+		return self.default_mongo_from_json('DEFAULT_MONGO_HOST')
+
+	def mongo_port(self):
+		return self.default_mongo_from_json('DEFAULT_MONGO_PORT')
+
+	def mongo_username(self):
+		return self.default_mongo_from_json('DEFAULT_MONGO_USERNAME')
+	
+	def mongo_password(self):
+		return self.default_mongo_from_json('DEFAULT_MONGO_PASSWORD')
+	
+
+if __name__ == '__main__':
+	ReadColsTest_obj = ReadCols_Class(param_columns_path = 'columns_json.json',
+					  param_add_columns_path = 'add_columns_json.json')
+	print(ReadColsTest_obj.columns_from_json)
+	print(type(ReadColsTest_obj.columns_from_json))
+	print(ReadColsTest_obj.add_columns_from_json)
+	print(type(ReadColsTest_obj.add_columns_from_json))
+	print(ReadColsTest_obj.columns_list())
+	print(ReadColsTest_obj.add_columns_list())
+
